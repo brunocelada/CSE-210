309384967529a0f75d1e85482c7f2f92c03cabbb
brunocelada <brunocelada4xd@gmail.com>
2023-04-20 14:03:07 -0300

    Preparation 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Console.WriteLine("Hello Prep1 World!");
+
+        // Asking the first and last names
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // Printing the "James Bond" phrase
+        Console.WriteLine($"\nYour name is {lastName}, {firstName} {lastName}.");
     }
 }
