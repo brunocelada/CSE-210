944bb72e2466795a7b86b2ec4b75e18321e9fd6a
brunocelada <brunocelada4xd@gmail.com>
2023-06-01 11:04:43 -0300

    W7 Inheritance Learning Activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Console.WriteLine("Hello Learning04 World!");
+
+        Assignment multip = new Assignment("Bruno", "Chemistry");
+        Console.WriteLine (multip.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment math1 = new MathAssignment("Doris", "Maths", "5.3", "10-13,14");
+        Console.WriteLine (math1.GetSummary());
+        Console.WriteLine (math1.GetHomeworkList());
+        Console.WriteLine();
+
+        WritingAssignment writing1 = new WritingAssignment("Javier", "Music", "Get the job done");
+        Console.WriteLine (writing1.GetSummary());
+        Console.WriteLine (writing1.GetWritingInformation());
+        Console.WriteLine();
     }
 }
--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -1,78 +1,92 @@
-class Scripture 
+class Scripture
 {
     private Reference _reference;
     private List<Word> _scripture;
 
     // Constructions
-    public Scripture ()
+    public Scripture()
     {
 
     }
-    public Scripture (Reference reference)
+    public Scripture(Reference reference)
     {
         _reference = reference;
     }
-    public Scripture (Reference reference, List<Word> scripture)
+    public Scripture(Reference reference, List<Word> scripture)
     {
         _reference = reference;
         _scripture = scripture;
     }
 
     // Modules 
-    public void SetScripture (List<Word> scripture, Reference reference){
+    public void SetScripture(List<Word> scripture, Reference reference)
+    {
         _scripture = scripture;
         _reference = reference;
-    }   
-    public void SetScriptureText (List<Word> scripture){
+    }
+    public void SetScriptureText(List<Word> scripture)
+    {
         _scripture = scripture;
     }
-    public void SetScriptureReference (Reference reference){
+    public void SetScriptureReference(Reference reference)
+    {
         _reference = reference;
     }
-    public string GetScripture (){
+    public string GetScripture()
+    {
         string text = "";
-        foreach (Word word in _scripture){
+        foreach (Word word in _scripture)
+        {
             text += word.GetWord();
             text += " ";
         }
         text = text.Trim();
         return $"{_reference.GetReference()} '{text}'";
     }
-    public string GetReference(){
+    public string GetReference()
+    {
         return _reference.GetReference();
     }
-    public bool Memorize (){
+    public bool Memorize()
+    {
         // Create a list of int that save the indexes of the words in the scripture that are not hidden yet.
         List<int> possibleWords = new List<int>();
 
         // Save the indexes of the displayed words.
-        for (int i = 0; i < _scripture.Count ; i++){
-            if (_scripture[i].GetStatus()){
-                possibleWords.Add (i);
+        for (int i = 0; i < _scripture.Count; i++)
+        {
+            if (_scripture[i].GetStatus())
+            {
+                possibleWords.Add(i);
             }
         }
 
         // If there are displayed words, change their status with the Hiddener method.
-        if (possibleWords.Count > 3){
+        if (possibleWords.Count > 3)
+        {
             // Hide 3 random words.
             Hiddener(3, possibleWords);
             return false;
         }
-        else if (possibleWords.Count > 0){
+        else if (possibleWords.Count > 0)
+        {
             // Hide the remaining words.
             Hiddener(possibleWords.Count, possibleWords);
             return false;
         }
         // If all the words are hidden, return true (to finish the program).
-        else{
+        else
+        {
             return true;
         }
     }
-    private void Hiddener (int times, List<int> possibleWords){
+    private void Hiddener(int times, List<int> possibleWords)
+    {
         Random random = new Random();
 
         // Randomly, choose a index of a non hidden word and change its status to hidden (false)
-        for (int i = 0; i < times; i++){
+        for (int i = 0; i < times; i++)
+        {
             int index = random.Next(0, possibleWords.Count);
 
             // Take and remove the index from the list
@@ -83,5 +97,5 @@ class Scripture
             _scripture[remove].ChangeStatus();
         }
     }
-  
+
 }
